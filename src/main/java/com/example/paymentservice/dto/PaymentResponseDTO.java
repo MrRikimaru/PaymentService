79185e217f846bb9605a575e3659ac0b5c94bcea
Andrey_Berg <berg.andrey@icloud.com>
2025-12-25 11:00:13 +0300

    add PaymentResponseDTO

---
+++ src/main/java/com/example/paymentservice/dto/PaymentResponseDTO.java
@@ -0,0 +1,26 @@
+package com.example.paymentservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentResponseDTO {
+    private String id;
+    private Long orderId;
+    private Long userId;
+    private String status;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone = "UTC")
+    private Instant timestamp;
+
+    private BigDecimal paymentAmount;
+}
