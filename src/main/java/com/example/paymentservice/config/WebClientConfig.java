9f28cc4be791035a7986088b373e4808b30762ee
Andrey_Berg <berg.andrey@icloud.com>
2026-01-07 21:10:25 +0300

    edit WebClientConfig

--- src/main/java/com/example/paymentservice/config/WebClientConfig.java
+++ src/main/java/com/example/paymentservice/config/WebClientConfig.java
@@ -1,15 +1,27 @@
 package com.example.paymentservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
 
+    @Value("${external.api.random-number.timeout:5000}")
+    private int timeout;
+
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(
+                        HttpClient.create()
+                                .responseTimeout(Duration.ofMillis(timeout))
+                ))
                 .build();
     }
 }
